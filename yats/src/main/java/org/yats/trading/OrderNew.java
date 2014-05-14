ca2c78f5493471c0e220e42771078d6b4f05753d
Emiliano Fraticelli <snowboard_extr3me@hotmail.it>
2014-05-14 11:10:06 +0200

    Update OrderNew.java
    TESTED

--- yats/src/main/java/org/yats/trading/OrderNew.java
+++ yats/src/main/java/org/yats/trading/OrderNew.java
@@ -144,6 +144,19 @@ public class OrderNew extends OrderBase {
         limit = Decimal.ZERO;
     }
 
+   public boolean isSameAs(OrderNew data) {
+
+        if(timestamp.compareTo(data.timestamp)!=0) return false;
+        if(externalAccount.compareTo(data.externalAccount)!=0) return false;
+        if(internalAccount.compareTo(data.internalAccount)!=0) return false;
+        if(!(bookSide.toDirection()==(data.bookSide.toDirection()))) return false;
+        if(!(size.isEqualTo(data.size))) return false;
+        if(!(limit.isEqualTo(data.limit))) return false;
+        if(productId.compareTo(data.productId)!=0) return false;
+
+        return true;
+    }
+
     private DateTime timestamp;
     private String externalAccount;
     private String internalAccount;
@@ -153,6 +166,7 @@ public class OrderNew extends OrderBase {
     private Decimal limit;
     private String productId;
 
+    
 
     private static class OrderNewNull extends OrderNew {
 
