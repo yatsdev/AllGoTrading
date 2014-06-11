098d70b22d3282bf624a8a1da993d02d23ae426a
yatsdev <yatsdev@hh2013.33mail.com>
2014-06-12 00:32:27 +0200

    SAP marketdata

--- yats/src/test/java/org/yats/trading/MarketDataTest.java
+++ yats/src/test/java/org/yats/trading/MarketDataTest.java
@@ -8,29 +8,32 @@ import org.yats.common.Decimal;
 
 public class MarketDataTest {
 
-    public static final String EUR_ID = "CCY007";
-    public static final String USD_ID = "CCY001";
-    public static final String CHF_ID = "CCY004";
+    public static final String EUR_PID = "CCY007";
+    public static final String USD_PID = "CCY001";
+    public static final String CHF_PID = "CCY004";
 
-    public static final String EURUSD_ID = "OANDA0001";
+    public static final String EURUSD_PID = "OANDA0001";
     public static final Decimal EUR_USD_LAST = Decimal.fromString("1.35");
 
-    public static final String USDCHF_ID = "OANDA0004";
+    public static final String USDCHF_PID = "OANDA0004";
     public static final Decimal USD_CHF_LAST = Decimal.fromString("0.9");
 
 
     public static final String SAP_PID ="4663789";
-    public static final Decimal SAP_EUR_LAST = Decimal.fromString("85");
+    public static final Decimal SAP_LAST = Decimal.fromString("85");
 
 
-    public final static MarketData EURUSD = new MarketData(DateTime.now(DateTimeZone.UTC),EURUSD_ID,
+    public final static MarketData EURUSD = new MarketData(DateTime.now(DateTimeZone.UTC), EURUSD_PID,
             EUR_USD_LAST.subtract(Decimal.CENT), EUR_USD_LAST.add(Decimal.CENT), EUR_USD_LAST,
             Decimal.ONE, Decimal.ONE, Decimal.ONE);
 
-    public final static MarketData USDCHF = new MarketData(DateTime.now(DateTimeZone.UTC),USDCHF_ID,
+    public final static MarketData USDCHF = new MarketData(DateTime.now(DateTimeZone.UTC), USDCHF_PID,
             USD_CHF_LAST.subtract(Decimal.CENT), USD_CHF_LAST.add(Decimal.CENT), USD_CHF_LAST,
             Decimal.ONE, Decimal.ONE, Decimal.ONE);
 
+    public final static MarketData SAP = new MarketData(DateTime.now(DateTimeZone.UTC), SAP_PID,
+            SAP_LAST.subtract(Decimal.CENT), SAP_LAST.add(Decimal.CENT), SAP_LAST,
+            Decimal.ONE, Decimal.ONE, Decimal.ONE);
 
     @Test
     public void can() {
--- yats/src/test/java/org/yats/trading/RateConverterTest.java
+++ yats/src/test/java/org/yats/trading/RateConverterTest.java
@@ -9,21 +9,21 @@ public class RateConverterTest {
 
 //    @Test
 //    public void canConvertPositionInEURToEUR() {
-//        Position p1InEUR = converter.convert(p1, MarketDataTest.EUR_ID);
-//        assert(p1InEUR.isSize(MarketDataTest.SAP_EUR_LAST.multiply(SAP_SIZE)));
+//        Position p1InEUR = converter.convert(p1, MarketDataTest.EUR_PID);
+//        assert(p1InEUR.isSize(MarketDataTest.SAP_LAST.multiply(SAP_SIZE)));
 //    }
 //
 //    @Test
 //    public void canConvertPositionInEURToUSD() {
-//        Position p1InUSD = converter.convert(p1, MarketDataTest.USD_ID);
-//        Decimal expectedSize = MarketDataTest.SAP_EUR_LAST.multiply(SAP_SIZE).multiply(MarketDataTest.EUR_USD_LAST);
+//        Position p1InUSD = converter.convert(p1, MarketDataTest.USD_PID);
+//        Decimal expectedSize = MarketDataTest.SAP_LAST.multiply(SAP_SIZE).multiply(MarketDataTest.EUR_USD_LAST);
 //        assert(p1InUSD.isSize(expectedSize));
 //    }
 //
 //    @Test
 //    public void canConvertPositionInEURToCHF() {
-//        Position p1InCHF = converter.convert(p1, MarketDataTest.CHF_ID);
-//        Decimal expectedSize = MarketDataTest.SAP_EUR_LAST
+//        Position p1InCHF = converter.convert(p1, MarketDataTest.CHF_PID);
+//        Decimal expectedSize = MarketDataTest.SAP_LAST
 //                .multiply(SAP_SIZE)
 //                .multiply(MarketDataTest.EUR_USD_LAST)
 //                .multiply(MarketDataTest.USD_CHF_LAST)
@@ -37,6 +37,7 @@ public class RateConverterTest {
         converter = new RateConverter(productList);
         converter.onMarketData(MarketDataTest.EURUSD);
         converter.onMarketData(MarketDataTest.USDCHF);
+        converter.onMarketData(MarketDataTest.SAP);
         p1 = new Position(MarketDataTest.SAP_PID, SAP_SIZE);
     }
 
