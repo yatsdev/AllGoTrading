1988de514152b2e6782962bb4dd045eb12444bdc
yatsdev <yatsdev@hh2013.33mail.com>
2014-09-03 21:23:16 +0200

    typo

--- yats/src/main/java/org/yats/trader/examples/strategies/Scalper.java
+++ yats/src/main/java/org/yats/trader/examples/strategies/Scalper.java
@@ -14,7 +14,7 @@ public class Scalper extends StrategyBase {
     // the configuration file log4j.properties for Log4J has to be provided in the working directory
     // an example of such a file is at config/log4j.properties.
     // if Log4J gives error message that it need to be configured, copy this file to the working directory
-    final Logger log = LoggerFactory.getLogger(MarketFollow.class);
+    final Logger log = LoggerFactory.getLogger(Scalper.class);
 
     @Override
     public void onPriceData(PriceData priceData)
