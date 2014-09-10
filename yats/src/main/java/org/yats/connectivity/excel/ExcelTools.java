7879af8e7c54b4cb4d848d24a814f06c5c2a57ae
yatsdev <yatsdev@hh2013.33mail.com>
2014-09-11 00:40:51 +0200

    encapsulated DDE client in class DDELink created interface for DDELink replaced coupling with DDE client within ExcelConnection with access to interface for encapsulated DDE client created mock object created unit test to show how mocking works. in this case reading and parsing the first row of Reports sheet

--- yats/src/main/java/org/yats/connectivity/excel/ExcelTools.java
+++ yats/src/main/java/org/yats/connectivity/excel/ExcelTools.java
@@ -1,8 +1,11 @@
 package org.yats.connectivity.excel;
 
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ExcelTools {
 
@@ -24,5 +27,43 @@ public class ExcelTools {
         return r1ColumnToVector;
     }
 
+    public void connect(String applicationname, String sheetname) {
+        ddeLink.connect(applicationname, sheetname);
+    }
+
+    public void readFirstRowFromDDE() {
+        String firstRow = ddeLink.request("R1");
+        parseFirstRow(firstRow);
+    }
+
+    public int countKeysInFirstRow(){
+        int count=0;
+        for(String s : keysInFirstRow) {
+            count+= s.isEmpty() ? 0 : 1;
+        }
+        return count;
+    }
+
+
+    private void parseFirstRow(String firstRow) {
+        String accountsString = firstRow.replace("\r\n", "\t");
+        String[] parts = accountsString.split("\t");
+        keysInFirstRow.clear();
+        mapOfFirstRowKeys.clear();
+        keysInFirstRow.addAll(Arrays.asList(parts));
+        for (String s : keysInFirstRow) mapOfFirstRowKeys.put(s, s);
+        if (keysInFirstRow.size() > 0) keysInFirstRow.remove(0);
+    }
+
+
+    public ExcelTools(IProvideDDEConversation _DDELink) {
+        ddeLink = _DDELink;
+        keysInFirstRow = new ArrayList<String>();
+        mapOfFirstRowKeys = new ConcurrentHashMap<String, String>();
+    }
+
+    private ArrayList<String> keysInFirstRow;
+    private ConcurrentHashMap<String,String> mapOfFirstRowKeys;
+    private final IProvideDDEConversation ddeLink;
 
 }
