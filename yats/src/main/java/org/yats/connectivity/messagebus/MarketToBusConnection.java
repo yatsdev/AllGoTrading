359df3535548c86570610f1ba2274ecdac9ffa8c
yatsdev <yatsdev@hh2013.33mail.com>
2014-11-01 20:01:15 +0100

    MarketToBus connection didnt startStrategy

--- yats/src/main/java/org/yats/connectivity/messagebus/MarketToBusConnection.java
+++ yats/src/main/java/org/yats/connectivity/messagebus/MarketToBusConnection.java
@@ -54,6 +54,7 @@ public class MarketToBusConnection extends StrategyBase implements IAmCalledBack
     public void onInitStrategy()
     {
         setInternalAccount("marketToBusConnection");
+        startStrategy();
     }
 
     @Override
