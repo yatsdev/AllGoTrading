3671977bdad9021ac04a088f00e709ead0eb5db6
yatsdev <yatsdev@hh2013.33mail.com>
2014-11-04 10:55:03 +0000

    esper

---
+++ yats/src/test/java/org/yats/common/EventTest.java
@@ -0,0 +1,18 @@
+package org.yats.common;
+
+import org.testng.annotations.Test;
+
+@Test(groups = { "inMemory" })
+public class EventTest {
+
+    public void can()
+    {
+    }
+
+    public void setUp() {
+
+    }
+
+
+
+} // class
