9800e2a0c38402bcde9651f9111bb623ce57d33f
NeoSephiroth <snowboard_extr3me@hotmail.it>
2014-12-01 10:31:04 +0000

    Added class PriceDataInDouble, used in PriceCheck strategy as an example for Esper

--- yats/src/main/java/org/yats/trader/examples/strategies/PriceCheck.java
+++ yats/src/main/java/org/yats/trader/examples/strategies/PriceCheck.java
@@ -1,19 +1,21 @@
 package org.yats.trader.examples.strategies;
 
 
+import com.espertech.esper.client.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yats.common.Decimal;
 import org.yats.common.IProvideProperties;
 import org.yats.trader.StrategyBase;
 import org.yats.trading.PriceData;
+import org.yats.trading.PriceDataInDoubleFormat;
 import org.yats.trading.Receipt;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class PriceCheck extends StrategyBase {
+public class PriceCheck extends StrategyBase{
 
     // the configuration file log4j.properties for Log4J has to be provided in the working directory
     // an example of such a file is at config/log4j.properties.
@@ -49,6 +51,8 @@ public class PriceCheck extends StrategyBase {
             setReport("lastBigChange", lastBigChange);
             sendReports();
 
+
+
         } else {
             dots++;
             if(dots>80) {
@@ -58,8 +62,14 @@ public class PriceCheck extends StrategyBase {
             System.out.print(".");
         }
         lastPrices.put(key, priceData);
-    }
 
+
+
+        PriceDataInDoubleFormat priceDataInDoubleFormat=new PriceDataInDoubleFormat(priceData);
+
+        cepRT.sendEvent(priceDataInDoubleFormat);
+
+    }
 
     @Override
     public void onReceiptForStrategy(Receipt receipt)
@@ -99,6 +109,7 @@ public class PriceCheck extends StrategyBase {
         upMove = Decimal.fromString(getConfig("upMove","1.000001"));
         downMove = Decimal.fromString(getConfig("downMove","0.999999"));
 
+
     }
 
     @Override
@@ -111,10 +122,22 @@ public class PriceCheck extends StrategyBase {
         super();
         lastPrice = PriceData.NULL;
         shuttingDown=false;
+
+        //The Configuration is meant only as an initialization-time object.
+        Configuration cepConfig = new Configuration();
+        cepConfig.addEventType("PriceDataInDoubleFormat", PriceDataInDoubleFormat.class.getName());
+        EPServiceProvider cep = EPServiceProviderManager.getProvider("myCEPEngine", cepConfig);
+        cepRT = cep.getEPRuntime();
+
+        EPAdministrator cepAdm = cep.getEPAdministrator();
+        EPStatement cepStatement = cepAdm.createEPL("select avg(ask) from PriceDataInDoubleFormat.win:time(3 min)"); //
+        cepStatement.addListener(new CEPListener());
+
+
     }
 
     private PriceData lastPrice;
-
+    private EPRuntime cepRT;
 
     private ConcurrentHashMap<String,PriceData> lastPrices=new ConcurrentHashMap();
     private boolean shuttingDown;
@@ -125,3 +148,10 @@ public class PriceCheck extends StrategyBase {
     private Decimal upMove;
     private Decimal downMove;
 } // class
+
+class CEPListener implements UpdateListener {
+
+    public void update(EventBean[] newData, EventBean[] oldData) {
+        System.out.println("Event received: " + newData[0].getUnderlying());
+    }
+}
